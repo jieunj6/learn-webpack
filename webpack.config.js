2e9aaec5fa5656c75f35c1d2e3f57f771002f0f8
jieunjeong <qpapple@naver.com>
2021-05-09 13:27:10 +0900

    플러그인 - HtmlWebpackPlugin, CleanWebpackPlugin, MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 // const MyPlugin = require("./src/myplugin"); //내가 만든 플러그인 예시
-const webpack = require('webpack'); // 배너 정보를 아래에 직접 입력할때 사용
-const banner = require("./banner.js"); // 배너 정보를 따로 불러올때 사용
-
+const webpack = require('webpack'); // 배너 정보를 아래에 직접 입력할때 사용 플러그인
+const banner = require("./banner.js"); // 배너 정보를 따로 불러올때 사용 플러그인
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // 이전 빌드파일 삭제 플러그인
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // css 추출 플러그인
 
 //module.exports = {
 module.exports = (env) => {
@@ -13,7 +14,7 @@ module.exports = (env) => {
         : './src/index.js';
 
     return {
-        mode: 'development',
+        mode: 'production',  // none, development, production
         entry: entryPath,
         output: {
             filename: 'bundle.js',
@@ -36,15 +37,25 @@ module.exports = (env) => {
             }),*/
             new webpack.BannerPlugin(banner), // 배너 불러오기 방식
             new HtmlWebpackPlugin({
-                // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
-                template: 'index.html',
-            }),
+                template: 'index.html', // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
+                hash: true, // 정적 파일을 불러올때 쿼리문자열에 웹팩 해쉬값을 추가한다
+           }),
+            new CleanWebpackPlugin(), // 이전 빌드 폴더에 불필요한 파일 다 삭제
+            ...(process.env.NODE_ENV === "production"
+                ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+                : []),
         ],
         module: {
             rules: [
                 {
                     test: /\.css$/,
-                    use: ['style-loader', 'css-loader']
+                    // use: ['style-loader', 'css-loader']
+                    use: [
+                        process.env.NODE_ENV === "production"
+                            ? MiniCssExtractPlugin.loader // 프로덕션 환경
+                            : "style-loader", // 개발 환경
+                        "css-loader",
+                    ],
                 },
                 /*{
                     test: /\.png$/, // .png 확장자로 마치는 모든 파일
